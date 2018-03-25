80ac517699eb4b58058d4b1688e5816706d3640b
Cesar <Cesar@CESAR-V5470>
2018-03-25 17:18:29 -0300

    cahuei

--- MineSweeper/src/Mines.java
+++ MineSweeper/src/Mines.java
@@ -5,7 +5,7 @@ public class Mines extends JFrame
     //Create new instance of MineFrame
     public static void main(String[] args)
     {
-        //caaassadadhuasisdfmkfjfeswuisadhfsaifwe
+        //rooolaaaaaaaaaaaalalalalasojasijdiashdaso
         new MineFrame();
     }
 }
